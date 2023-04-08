3830dfc97d117cedbe039c0d5eb00f31388cb6d4
Yu S. Huang <polyactis@gmail.com>
2023-04-08 19:33:00 +0800

    test a println() with or without semicolon

--- c6_enum_match/src/main.rs
+++ c6_enum_match/src/main.rs
@@ -34,7 +34,7 @@ fn value_in_cents(coin: &Coin) -> u8 {
 fn main() {
     println!("Hello, world!");
 
-    let dice_roll = 9;
+    let dice_roll = 3;
     match dice_roll {
         3 => add_fancy_hat(),
         7 => remove_fancy_hat(),
@@ -76,8 +76,12 @@ fn main() {
 
 
 
-fn add_fancy_hat() {}
-fn remove_fancy_hat() {}
+fn add_fancy_hat() {
+    println!("Added a fancy hat!");
+}
+fn remove_fancy_hat() {
+    println!("Removed a fancy hat!")
+}
 
 fn plus_one(x: Option<i32>) -> Option<i32> {
     match x {
