a88525b6fde1df7efb21520db12194fca8aa6545
Yu S. Huang <polyactis@gmail.com>
2023-04-25 13:47:02 +0800

    Chap 10.1 generic for function, struct, method

---
+++ c10_generic_trait/src/main.rs
@@ -0,0 +1,75 @@
+use std::fmt::Debug;
+
+fn largest<T: std::cmp::PartialOrd>(list: &[T]) -> &T {
+    let mut largest = &list[0];
+
+    for item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
+#[derive(Debug)]
+struct Point<T, U> {
+    x: T,
+    y: U,
+}
+
+
+impl<T,U> Point<T,U> {
+    fn x(&self) -> &T {
+        &self.x
+    }
+}
+
+impl Point<f32, f32> {
+    fn distance_from_origin(&self) -> f32 {
+        (self.x.powi(2) + self.y.powi(2)).sqrt()
+    }
+}
+
+impl Point<i32, i32> {
+    fn distance_from_origin(&self) -> f32 {
+        ((self.x as f32).powi(2) + (self.y as f32).powi(2)).sqrt()
+    }
+}
+
+
+impl<X1, Y1> Point<X1, Y1> {
+    fn mixup<X2, Y2>(self, other: Point<X2, Y2>) -> Point<X1, Y2> {
+        Point {
+            x: self.x,
+            y: other.y,
+        }
+    }
+}
+
+fn main() {
+    let number_list = vec![34, 50, 25, 100, 65];
+
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
+
+    let char_list = vec!['y', 'm', 'a', 'q'];
+
+    let result = largest(&char_list);
+    println!("The largest char is {}", result);
+
+    let both_integer = Point { x: 5, y: 10 };
+    let both_float = Point { x: 1.0, y: 4.0 };
+    let integer_and_float = Point { x: 5, y: 4.0 };
+    println!("x of {:?} is {}.", integer_and_float, integer_and_float.x());
+    println!("For {:?}, Distance from the origin is {}.", both_float, both_float.distance_from_origin());
+
+    println!("For {:?}, Distance from the origin is {}.", both_integer, both_integer.distance_from_origin());
+
+    let p1 = Point { x: 5, y: 10.4 };
+    let p2 = Point { x: "Hello", y: 'c' };
+    let p3 = p1.mixup(p2);
+
+    println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
+
+}
