85cf48998cd4e5264bd81535d758e3dd56b7c69c
Yu S. Huang <polyactis@gmail.com>
2023-05-29 18:20:10 +0800

    c11 automated tests

---
+++ c11_test/README.md
@@ -0,0 +1,6 @@
+
+Run "cargo test" to run all tests in module tests but test functions in main.rs won't be run.
+
+Run "cargo test greater_than_100" to run the specific test.
+
+Run "cargo test two_plus_two_in_main" to run test functions in main.rs.
---
+++ c11_test/src/main.rs
@@ -0,0 +1,13 @@
+
+
+#[test]
+fn two_plus_two_in_main() {
+    // This test won't be run during "cargo test".
+    // Explicitly running "cargo test two_plus_two_in_main" will run it.
+    // This function can be invoked in main().
+    assert_eq!(2 + 2, 4);
+}
+
+fn main() {
+    println!("Hello, world!");
+}
