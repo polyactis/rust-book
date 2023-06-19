6ff87afec26a5764b7735c9ecb7531043bcd05d1
Yu S. Huang <polyactis@gmail.com>
2023-06-19 19:10:32 +0800

    split most main.rs code into lib.rs

--- c12_minigrep/src/main.rs
+++ c12_minigrep/src/main.rs
@@ -1,30 +1,19 @@
 use std::env;
-use std::fs;
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let file_path = args[2].clone();
-
-    Config {query, file_path}
-}
+use std::process;
+use c12_minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = parse_config(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}. {} arguments, but need 2.",
+            args.len()-1);
+        process::exit(1);
+    });
+
+    if let Err(e) = c12_minigrep::run(config) {
+        println!("Problem in searching through the file .. {e}.");
+        process::exit(2);
+    }
 
 
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.file_path);
-
-
-    let contents: String = fs::read_to_string(config.file_path)
-        .expect("Should have been able to read the file");
-
-    println!("With text:\n{contents}");
 }
