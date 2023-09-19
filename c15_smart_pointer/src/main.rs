d02c35594349dbd965c73bdbe8317047f4b32e70
Yu S. Huang <polyactis@gmail.com>
2023-09-19 15:07:22 +0800

    Use Rc instead of Box

--- c15_smart_pointer/src/main.rs
+++ c15_smart_pointer/src/main.rs
@@ -1,12 +1,18 @@
 #[derive(Debug)]
 enum List {
-    Cons(i32, Box<List>),
+    Cons(i32, Rc<List>),
     Nil,
 }
 
+/*
+enum List {
+    Cons(i32, Box<List>),
+    Nil,
+}
+*/
 
 use crate::List::{Cons, Nil};
-use std::ops::Deref;
+use std::{ops::Deref, rc::Rc};
 
 struct MyBox<T>(T);
 
@@ -40,8 +46,8 @@ impl Drop for CustomSmartPointer {
 }
 
 fn main() {
-    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
-    println!("{:?}", list);
+    //let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+    //println!("{:?}", list);
 
     let x = 5;
     //let y = &x;
@@ -68,7 +74,17 @@ fn main() {
         data: String::from("some data"),
     };
     println!("CustomSmartPointer created.");
-    //c.drop();
+    // c.drop(); //explicit destructor calls not allowed
     drop(c);
     println!("CustomSmartPointer dropped before the end of main.");
+
+    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    println!("strong count of a after creating a = {}", Rc::strong_count(&a));
+    let b = Cons(3, Rc::clone(&a));
+    println!("strong count of a after creating b = {}", Rc::strong_count(&a));
+    {
+        let c = Cons(4, Rc::clone(&a));
+        println!("strong count of a after creating c = {}", Rc::strong_count(&a));
+    }
+    println!("strong count of a after c goes out of scope = {}", Rc::strong_count(&a));
 }
