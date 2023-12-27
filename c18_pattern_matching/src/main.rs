4127d6825107883fa3e2a8e273182d6e19014d13
Yu S. Huang <polyactis@gmail.com>
2023-12-27 21:42:01 +0800

    initial code for c18 patterns and matching

---
+++ c18_pattern_matching/src/main.rs
@@ -0,0 +1,31 @@
+fn main() {
+    let mut x = Some(1);
+    //let mut x: Option<i32>  = None;
+    println!("i={:?}", x);
+    //let x = ();
+    x = match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    };
+    println!("i={:?}", x);
+
+    println!("Hello, world!");
+
+    let favorite_color: Option<&str> = None;
+    let is_tuesday = false;
+    let age: Result<u8, _> = "34".parse();
+
+    if let Some(color) = favorite_color {
+        println!("Using your favorite color, {color}, as the background");
+    } else if is_tuesday {
+        println!("Tuesday is green day!");
+    } else if let Ok(age) = age {
+        if age > 30 {
+            println!("Using purple as the background color");
+        } else {
+            println!("Using orange as the background color");
+        }
+    } else {
+        println!("Using blue as the background color");
+    }
+}
