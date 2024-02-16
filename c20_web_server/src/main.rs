c14a26b23753598156a2ef894026b4cb23c0a4cb
Yu S. Huang <polyactis@gmail.com>
2024-02-16 21:09:08 +0800

    c20: spawn a thread for each request (OS can run out of resources)

--- c20_web_server/src/main.rs
+++ c20_web_server/src/main.rs
@@ -1,9 +1,10 @@
-use std::{fs, io::{BufRead, BufReader, Write}, net::{TcpListener, TcpStream}};
+use std::{fs, io::{BufRead, BufReader, Write}, net::{TcpListener, TcpStream}, thread, time::Duration};
 
 
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
-    let request_line = buf_reader.lines().next().unwrap().unwrap();
+    let request_line: String = buf_reader.lines().next().unwrap().unwrap();
+    eprint!("Handling request: {request_line}");
 
     // let http_request: Vec<_> = buf_reader
     //     .lines()
@@ -12,24 +13,29 @@ fn handle_connection(mut stream: TcpStream) {
     //     .collect();
     // println!("Request: {:#?}", http_request);
 
-
-    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
-        ("HTTP/1.1 200 OK", "hello.html")
-    } else {
-        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    // must add & in front of request_line due to match unable to auto referencing and derefencing.
+    let (status_line, filename) = match &request_line[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "hello.html")},
+        _ => ("HTTP/1.1 404 NOT FOUND", "404.html")
     };
     let contents = fs::read_to_string(filename).unwrap();
     let length = contents.len();
     let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
-    println!("Handling request {request_line}");
     stream.write_all(response.as_bytes()).unwrap();
+    eprintln!(" Done.");
 }
+
 fn main() {
-    println!("{:#?}", &[0; 3]);
+    // [0; 3] is [0,0,0];
+    //println!("{:#?}", &[0; 3]);
+    println!("Listening at TCP 127.0.0.1:7878.");
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        handle_connection(stream);
+        thread::spawn(|| { handle_connection(stream); });
     }
 }
